fd6f252071e2a44ea6014731c6069babcfbc8470
Вячеслав Попенко <slavko.popenko@gmail.com>
2026-09-04 12:01:10 +0300

    Add files via upload

---
+++ less2/script.js
@@ -0,0 +1,71 @@
+// > < >= <= == === !== !=
+
+// let a, b;
+// a = '1';
+// b = 1;
+// console.log(a === b);
+
+// if (умова){
+//     код
+// } else {
+//     код
+// }
+
+// let temp = Number(prompt('Enter temperature: '));
+// let result;
+// if (temp < 0){
+//     result = 'Cold'
+// } else if (temp > 0 && temp < 25){
+//     result = 'Norm'
+// } else if (temp >= 25){
+//     result = 'Hot'
+// }
+// alert(result);
+
+// 559689249825482589 ZADA4A!!!!! 41413414
+// let a = Number(prompt("Enter a number"));
+// let result;
+// if (a % 2 === 0) {
+//     result = 'Parne';
+// } else if (a % 2 === 1) {
+//     result = 'Ne parne';
+// }
+// alert(result)
+// ___________________________________________
+
+// const login = 'admin';
+// const password = '12345';
+//
+// let user_login = prompt('Enter your username');
+// let user_password = prompt('Enter your password');
+//
+// if (user_login === login && user_password === password) {
+//     alert("You're logged in!");
+// } else{
+//     alert('Access denied!');
+// }
+
+// let deliveryType = prompt('Please enter a delivery type'), cost=0;
+// switch (deliveryType) {
+//     case 'post': cost=100; break;
+//     case 'courier': cost=200; break;
+//     case 'pickup': cost=0; break;
+//     default: cost='n/a'; break;
+// }
+// alert(`cost is ${cost}`);
+
+// 59030VT03-T8938T0845948UGJI8UJVTIUU398T983U9T8VUVT9OIVHJ3V3ITJ3
+// let day = Number(prompt('What number of day is it?'))
+// let result;
+// switch(day) {
+//     case 1: result = 'Monday'; break;
+//     case 2: result = 'Tuesday'; break;
+//     case 3: result = 'Wednesday'; break;
+//     case 4: result = 'Thursday'; break;
+//     case 5: result = 'Friday'; break;
+//     case 6: result = 'Saturday'; break;
+//     case 7: result = 'Sunday'; break;
+//     default: result = 'No such day'; break;
+// }
+// alert(`Its ${result}`);
+// ________________________________________________________________
